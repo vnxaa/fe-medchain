23d26512efe60b558b166476f728290f9afd43f7
temtem2906 <temtem2906@example.com>
2023-08-07 09:26:51 +0700

    fix-account-request-page

--- pages/Register/RequestAccount.js
+++ pages/Register/RequestAccount.js
@@ -146,9 +146,12 @@ const RequestAccount = () => {
                     id="role"
                     value={role}
                     onChange={handleRoleChange}
+                    required
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   >
-                    <option value="">Chọn vai trò</option>
+                    <option disabled value="">
+                      Chọn vai trò
+                    </option>
                     <option value="doctor">Bác sĩ</option>
                     <option value="staff">Nhân viên</option>
                   </select>
